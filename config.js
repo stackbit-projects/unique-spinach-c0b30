142414c06bc6cba31b2de4d5e3be588f282da64e
Stackbit Code Editor <projects@stackbit.com>
2021-03-25 04:06:16 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Grayscale', // <title>
+  siteTitle: 'Kitt', // <title>
   manifestName: 'Grayscale',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
