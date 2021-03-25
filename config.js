d27bcb1f92e61fd02b560509989ecd3f434bdb59
Stackbit Code Editor <projects@stackbit.com>
2021-03-25 04:06:43 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: ', // <title>
+  siteTitle: '', // <title>
   manifestName: 'Grayscale',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
