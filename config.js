238a1c0c429f0f8e60d8743f43c5be5faf152d31
Stackbit Code Editor <projects@stackbit.com>
2021-03-25 04:46:24 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'Kittika's Rage Kittchen', // <title>
+  siteTitle: 'Kittika'', // <title>
   manifestName: 'Grayscale',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
